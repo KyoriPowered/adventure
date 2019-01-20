3c0391d76743fe5fa4d7b876ce85fb44dad6922d
kashike <kashike@vq.lc>
2019-01-20 13:52:04 -0800

    2019

--- src/main/java/net/kyori/text/serializer/ComponentSerializers.java
+++ src/main/java/net/kyori/text/serializer/ComponentSerializers.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of text, licensed under the MIT License.
  *
- * Copyright (c) 2017-2018 KyoriPowered
+ * Copyright (c) 2017-2019 KyoriPowered
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
