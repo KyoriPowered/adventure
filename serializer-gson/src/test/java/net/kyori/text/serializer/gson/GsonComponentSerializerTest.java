fd9e7679284367d28399c1176b0bafff5fa7301e
kashike <kashike@vq.lc>
2019-01-21 09:40:53 -0800

    add a few tests

--- serializer-gson/src/test/java/net/kyori/text/serializer/gson/GsonComponentSerializerTest.java
+++ serializer-gson/src/test/java/net/kyori/text/serializer/gson/GsonComponentSerializerTest.java
@@ -23,6 +23,9 @@
  */
 package net.kyori.text.serializer.gson;
 
+import net.kyori.text.KeybindComponent;
+import net.kyori.text.ScoreComponent;
+import net.kyori.text.SelectorComponent;
 import net.kyori.text.TextComponent;
 import net.kyori.text.TranslatableComponent;
 import net.kyori.text.event.ClickEvent;
@@ -34,6 +37,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GsonComponentSerializerTest {
+  @Test
+  void testDeserializeKeybind() {
+    assertEquals(KeybindComponent.of("key.jump"), GsonComponentSerializer.INSTANCE.deserialize("{\"keybind\":\"key.jump\"}"));
+  }
+
+  @Test
+  void testDeserializeScore() {
+    assertEquals(ScoreComponent.of("abc", "def"), GsonComponentSerializer.INSTANCE.deserialize("{\"score\":{\"name\":\"abc\",\"objective\":\"def\"}}"));
+  }
+
+  @Test
+  void testDeserializeSelector() {
+    assertEquals(SelectorComponent.of("@p"), GsonComponentSerializer.INSTANCE.deserialize("{\"selector\":\"@p\"}"));
+  }
+
+  @Test
+  void testDeserializeText() {
+    assertEquals(TextComponent.of("Hello, world."), GsonComponentSerializer.INSTANCE.deserialize("{\"text\":\"Hello, world.\"}"));
+    assertEquals(TextComponent.builder("c")
+      .color(TextColor.GOLD)
+      .append(TextComponent.of("o", TextColor.DARK_AQUA))
+      .append(TextComponent.of("l", TextColor.LIGHT_PURPLE))
+      .append(TextComponent.of("o", TextColor.DARK_PURPLE))
+      .append(TextComponent.of("u", TextColor.BLUE))
+      .append(TextComponent.of("r", TextColor.DARK_GREEN))
+      .append(TextComponent.of("s", TextColor.RED))
+      .build(), GsonComponentSerializer.INSTANCE.deserialize("[{\"text\":\"c\",\"color\":\"gold\"},{\"text\":\"o\",\"color\":\"dark_aqua\"},{\"text\":\"l\",\"color\":\"light_purple\"},{\"text\":\"o\",\"color\":\"dark_purple\"},{\"text\":\"u\",\"color\":\"blue\"},{\"text\":\"r\",\"color\":\"dark_green\"},{\"text\":\"s\",\"color\":\"red\"}]"));
+    assertEquals(TextComponent.builder("This is a test.")
+      .color(TextColor.DARK_PURPLE)
+      .hoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.of("A test.")))
+      .append(TextComponent.of(" "))
+      .append(TextComponent.of("A what?", TextColor.DARK_AQUA))
+      .build(), GsonComponentSerializer.INSTANCE.deserialize("[{\"text\":\"This is a test.\",\"color\":\"dark_purple\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"A test.\"}}},{\"text\":\" \"},{\"text\":\"A what?\",\"color\":\"dark_aqua\"}]"));
+  }
+
+  @Test
+  void testDeserializeTranslatable() {
+    assertEquals(TranslatableComponent.of("multiplayer.player.left"), GsonComponentSerializer.INSTANCE.deserialize("{\"translate\":\"multiplayer.player.left\"}"));
+    assertEquals(TranslatableComponent.builder("multiplayer.player.left")
+      .args(TextComponent.of("kashike"))
+      .build(), GsonComponentSerializer.INSTANCE.deserialize("{\"translate\":\"multiplayer.player.left\",\"with\":[{\"text\":\"kashike\"}]}"));
+  }
+
   @Test
   void testSerializeDeserialize() {
     final TextComponent expected = TextComponent.builder()
@@ -54,7 +100,7 @@ class GsonComponentSerializerTest {
       TextComponent.builder("kashike")
         .clickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg kashike "))
         .hoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ENTITY, TextComponent.of("{\"name\":\"kashike\",\"id\":\"eb121687-8b1a-4944-bd4d-e0a818d9dfe2\"}")))
-      .build()
+        .build()
     ).color(TextColor.YELLOW);
     assertEquals(component, GsonComponentSerializer.INSTANCE.deserialize(GsonComponentSerializer.INSTANCE.serialize(component)));
   }
