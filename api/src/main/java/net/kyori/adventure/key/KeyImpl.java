9a08c00025379f21af6de9e22d112bafe20c2de2
kashike <kashike@vq.lc>
2020-07-14 16:39:16 -0700

    oops

--- api/src/main/java/net/kyori/adventure/key/KeyImpl.java
+++ api/src/main/java/net/kyori/adventure/key/KeyImpl.java
@@ -108,7 +108,7 @@ import org.checkerframework.checker.nullness.qual.NonNull;
     return clampCompare(this.namespace.compareTo(that.namespace()));
   }
 
-  static int clampCompare(final int value) {
+  /* package */ static int clampCompare(final int value) {
     if(value < 0) return -1;
     if(value > 0) return 1;
     return value;
