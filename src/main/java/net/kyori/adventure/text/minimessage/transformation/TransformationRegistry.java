4106456d3993b775d096cd8f35e9d429a6876c11
Riley Park <riley.park@meino.net>
2020-10-22 10:44:48 -0700

    avoid reflection in TransformationRegistry

--- src/main/java/net/kyori/adventure/text/minimessage/transformation/TransformationRegistry.java
+++ src/main/java/net/kyori/adventure/text/minimessage/transformation/TransformationRegistry.java
@@ -1,42 +1,50 @@
 package net.kyori.adventure.text.minimessage.transformation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import net.kyori.adventure.text.minimessage.parser.Token;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-
 public class TransformationRegistry {
+  private final List<Entry<? extends Transformation>> transformations = new ArrayList<>();
 
-    private final Map<Class<? extends Transformation>, Function<String, Boolean>> known = new HashMap<>();
+  public TransformationRegistry() {
+    this.register(ColorTransformation::new, ColorTransformation::isApplicable);
+    this.register(DecorationTransformation::new, DecorationTransformation::isApplicable);
+    this.register(HoverTransformation::new, HoverTransformation::isApplicable);
+    this.register(ClickTransformation::new, ClickTransformation::isApplicable);
+    this.register(KeybindTransformation::new, KeybindTransformation::isApplicable);
+    this.register(TranslatableTransformation::new, TranslatableTransformation::isApplicable);
+    this.register(InsertionTransformation::new, InsertionTransformation::isApplicable);
+    this.register(FontTransformation::new, FontTransformation::isApplicable);
+    this.register(GradientTransformation::new, GradientTransformation::isApplicable);
+    this.register(RainbowTransformation::new, RainbowTransformation::isApplicable);
+  }
 
-    public TransformationRegistry() {
-        known.put(ColorTransformation.class, ColorTransformation::isApplicable);
-        known.put(DecorationTransformation.class, DecorationTransformation::isApplicable);
-        known.put(HoverTransformation.class, HoverTransformation::isApplicable);
-        known.put(ClickTransformation.class, ClickTransformation::isApplicable);
-        known.put(KeybindTransformation.class, KeybindTransformation::isApplicable);
-        known.put(TranslatableTransformation.class, TranslatableTransformation::isApplicable);
-        known.put(InsertionTransformation.class, InsertionTransformation::isApplicable);
-        known.put(FontTransformation.class, FontTransformation::isApplicable);
-        known.put(GradientTransformation.class, GradientTransformation::isApplicable);
-        known.put(RainbowTransformation.class, RainbowTransformation::isApplicable);
+  private <T extends Transformation> void register(final Supplier<T> factory, final Predicate<String> applicable) {
+    this.transformations.add(new Entry<>(applicable, factory));
+  }
+
+  public Transformation get(final String name, final List<Token> inners) {
+    for(final Entry<? extends Transformation> entry : this.transformations) {
+      if(entry.applicable.test(name)) {
+        final Transformation transformation = entry.factory.get();
+        transformation.load(name, inners);
+        return transformation;
+      }
     }
 
-    public Transformation get(String name, List<Token> inners) {
-        for (Map.Entry<Class<? extends Transformation>, Function<String, Boolean>> entry : known.entrySet()) {
-            if (entry.getValue().apply(name)) {
-                try {
-                    Transformation transformation = entry.getKey().newInstance();
-                    transformation.load(name, inners);
-                    return transformation;
-                } catch (InstantiationException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    return null;
+  }
 
-        return null;
+  static class Entry<T extends Transformation> {
+    final Predicate<String> applicable;
+    final Supplier<T> factory;
+
+    Entry(final Predicate<String> applicable, final Supplier<T> factory) {
+      this.applicable = applicable;
+      this.factory = factory;
     }
+  }
 }
