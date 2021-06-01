bcd67971ffd3cb87c8d253eafe84652d2bdd5aae
Callum Seabrook <callum.seabrook@prevarinite.com>
2021-06-01 19:17:03 +0100

    Fixed donkey formatting

--- api/src/test/java/net/kyori/adventure/bossbar/BossBarTest.java
+++ api/src/test/java/net/kyori/adventure/bossbar/BossBarTest.java
@@ -24,16 +24,10 @@
 package net.kyori.adventure.bossbar;
 
 import com.google.common.collect.ImmutableSet;
-import java.util.Collections;
-import java.util.Locale;
+import com.google.common.collect.Sets;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-
-import com.google.common.collect.Sets;
-import net.kyori.adventure.bossbar.BossBar.Color;
-import net.kyori.adventure.bossbar.BossBar.Flag;
-import net.kyori.adventure.bossbar.BossBar.Overlay;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.ComponentLike;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -41,8 +35,11 @@ import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 import static java.util.Collections.emptySet;
-import static net.kyori.adventure.bossbar.BossBar.bossBar;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BossBarTest {
   private final AtomicInteger name = new AtomicInteger();
@@ -62,26 +59,26 @@ public class BossBarTest {
     }
 
     @Override
-    public void bossBarColorChanged(final @NonNull BossBar bar, final @NonNull Color oldColor, final @NonNull Color newColor) {
+    public void bossBarColorChanged(final @NonNull BossBar bar, final BossBar.@NonNull Color oldColor, final BossBar.@NonNull Color newColor) {
       BossBarTest.this.color.incrementAndGet();
     }
 
     @Override
-    public void bossBarOverlayChanged(final @NonNull BossBar bar, final @NonNull Overlay oldOverlay, final @NonNull Overlay newOverlay) {
+    public void bossBarOverlayChanged(final @NonNull BossBar bar, final BossBar.@NonNull Overlay oldOverlay, final BossBar.@NonNull Overlay newOverlay) {
       BossBarTest.this.overlay.incrementAndGet();
     }
 
     @Override
-    public void bossBarFlagsChanged(final @NonNull BossBar bar, final @NonNull Set<Flag> oldFlags, final @NonNull Set<Flag> newFlags) {
+    public void bossBarFlagsChanged(final @NonNull BossBar bar, final @NonNull Set<BossBar.Flag> oldFlags, final @NonNull Set<BossBar.Flag> newFlags) {
       BossBarTest.this.flags.incrementAndGet();
     }
   };
-  private final BossBar bar = bossBar((ComponentLike) Component.empty(), 1f, Color.PURPLE, Overlay.PROGRESS);
+  private final BossBar bar = BossBar.bossBar((ComponentLike) Component.empty(), 1f, BossBar.Color.PURPLE, BossBar.Overlay.PROGRESS);
 
   @Test
   void testOfFlags() {
-    final BossBar bar = bossBar(Component.empty(), 1f, Color.PURPLE, Overlay.PROGRESS, ImmutableSet.of(Flag.DARKEN_SCREEN));
-    assertThat(bar.flags()).containsExactly(Flag.DARKEN_SCREEN);
+    final BossBar bar = BossBar.bossBar(Component.empty(), 1f, BossBar.Color.PURPLE, BossBar.Overlay.PROGRESS, ImmutableSet.of(BossBar.Flag.DARKEN_SCREEN));
+    assertThat(bar.flags()).containsExactly(BossBar.Flag.DARKEN_SCREEN);
   }
 
   // ====
@@ -116,42 +113,42 @@ public class BossBarTest {
   void testProgress_outOfRange() {
     assertThrows(IllegalArgumentException.class, () -> this.bar.progress(-1f));
     assertThrows(IllegalArgumentException.class, () -> this.bar.progress(1.1f));
-    assertThrows(IllegalArgumentException.class, () -> bossBar(Component.empty(), 2F, Color.WHITE, Overlay.PROGRESS));
-    assertThrows(IllegalArgumentException.class, () -> bossBar(Component.empty(), 2F, Color.WHITE, Overlay.PROGRESS, emptySet()));
+    assertThrows(IllegalArgumentException.class, () -> BossBar.bossBar(Component.empty(), 2F, BossBar.Color.WHITE, BossBar.Overlay.PROGRESS));
+    assertThrows(IllegalArgumentException.class, () -> BossBar.bossBar(Component.empty(), 2F, BossBar.Color.WHITE, BossBar.Overlay.PROGRESS, emptySet()));
   }
 
   @Test
   void testColor() {
-    assertEquals(Color.PINK, this.bar.color(Color.PINK).color());
+    assertEquals(BossBar.Color.PINK, this.bar.color(BossBar.Color.PINK).color());
     assertEquals(0, this.color.get());
 
     this.bar.addListener(this.listener);
-    assertEquals(Color.PURPLE, this.bar.color(Color.PURPLE).color());
+    assertEquals(BossBar.Color.PURPLE, this.bar.color(BossBar.Color.PURPLE).color());
     assertEquals(1, this.color.get());
 
-    assertEquals(Color.PURPLE, this.bar.color(Color.PURPLE).color());
+    assertEquals(BossBar.Color.PURPLE, this.bar.color(BossBar.Color.PURPLE).color());
     assertEquals(1, this.color.get()); // value has not changed, should not have incremented
   }
 
   @Test
   void testOverlay() {
-    assertEquals(Overlay.NOTCHED_6, this.bar.overlay(Overlay.NOTCHED_6).overlay());
+    assertEquals(BossBar.Overlay.NOTCHED_6, this.bar.overlay(BossBar.Overlay.NOTCHED_6).overlay());
     assertEquals(0, this.overlay.get());
 
     this.bar.addListener(this.listener);
-    assertEquals(Overlay.PROGRESS, this.bar.overlay(Overlay.PROGRESS).overlay());
+    assertEquals(BossBar.Overlay.PROGRESS, this.bar.overlay(BossBar.Overlay.PROGRESS).overlay());
     assertEquals(1, this.overlay.get());
 
-    assertEquals(Overlay.PROGRESS, this.bar.overlay(Overlay.PROGRESS).overlay());
+    assertEquals(BossBar.Overlay.PROGRESS, this.bar.overlay(BossBar.Overlay.PROGRESS).overlay());
     assertEquals(1, this.overlay.get()); // value has not changed, should not have incremented
   }
 
   @Test
   void testFlags() {
     // Ensure bar contains the flags we set
-    final BossBar bar = bossBar(Component.empty(), 0.5F, Color.WHITE, Overlay.PROGRESS, Sets.newHashSet(Flag.DARKEN_SCREEN));
-    assertTrue(bar.hasFlag(Flag.DARKEN_SCREEN));
-    assertFalse(bar.hasFlag(Flag.PLAY_BOSS_MUSIC));
+    final BossBar bar = BossBar.bossBar(Component.empty(), 0.5F, BossBar.Color.WHITE, BossBar.Overlay.PROGRESS, Sets.newHashSet(BossBar.Flag.DARKEN_SCREEN));
+    assertTrue(bar.hasFlag(BossBar.Flag.DARKEN_SCREEN));
+    assertFalse(bar.hasFlag(BossBar.Flag.PLAY_BOSS_MUSIC));
     assertNotNull(bar.addFlags());
 
     assertEquals(ImmutableSet.of(), this.bar.flags(ImmutableSet.of()).flags());
@@ -164,15 +161,15 @@ public class BossBarTest {
 
     // set flags to new from empty
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.DARKEN_SCREEN), this.bar.flags(ImmutableSet.of(Flag.DARKEN_SCREEN)).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.DARKEN_SCREEN), this.bar.flags(ImmutableSet.of(BossBar.Flag.DARKEN_SCREEN)).flags());
       assertEquals(1, this.flags.get());
-      assertThat(changes.flagsAdded.get()).containsExactly(Flag.DARKEN_SCREEN);
+      assertThat(changes.flagsAdded.get()).containsExactly(BossBar.Flag.DARKEN_SCREEN);
       assertThat(changes.flagsRemoved.get()).isEmpty();
     });
 
     // set flags to same as existing
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.DARKEN_SCREEN), this.bar.flags(ImmutableSet.of(Flag.DARKEN_SCREEN)).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.DARKEN_SCREEN), this.bar.flags(ImmutableSet.of(BossBar.Flag.DARKEN_SCREEN)).flags());
       assertEquals(1, this.flags.get()); // value has not changed, should not have incremented
       assertThat(changes.flagsAdded.get()).isEmpty();
       assertThat(changes.flagsRemoved.get()).isEmpty();
@@ -180,54 +177,54 @@ public class BossBarTest {
 
     // set flags to new from existing
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC), this.bar.flags(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC)).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC), this.bar.flags(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC)).flags());
       assertEquals(2, this.flags.get());
-      assertThat(changes.flagsAdded.get()).containsExactly(Flag.PLAY_BOSS_MUSIC);
-      assertThat(changes.flagsRemoved.get()).containsExactly(Flag.DARKEN_SCREEN);
+      assertThat(changes.flagsAdded.get()).containsExactly(BossBar.Flag.PLAY_BOSS_MUSIC);
+      assertThat(changes.flagsRemoved.get()).containsExactly(BossBar.Flag.DARKEN_SCREEN);
     });
 
     // add flags
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC, Flag.CREATE_WORLD_FOG), this.bar.addFlag(Flag.CREATE_WORLD_FOG).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC, BossBar.Flag.CREATE_WORLD_FOG), this.bar.addFlag(BossBar.Flag.CREATE_WORLD_FOG).flags());
       assertEquals(3, this.flags.get());
-      assertThat(changes.flagsAdded.get()).containsExactly(Flag.CREATE_WORLD_FOG);
+      assertThat(changes.flagsAdded.get()).containsExactly(BossBar.Flag.CREATE_WORLD_FOG);
       assertThat(changes.flagsRemoved.get()).isEmpty();
     });
-    assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC), this.bar.flags(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC)).flags());
+    assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC), this.bar.flags(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC)).flags());
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC, Flag.CREATE_WORLD_FOG), this.bar.addFlags(Flag.CREATE_WORLD_FOG).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC, BossBar.Flag.CREATE_WORLD_FOG), this.bar.addFlags(BossBar.Flag.CREATE_WORLD_FOG).flags());
       assertEquals(5, this.flags.get());
-      assertThat(changes.flagsAdded.get()).containsExactly(Flag.CREATE_WORLD_FOG);
+      assertThat(changes.flagsAdded.get()).containsExactly(BossBar.Flag.CREATE_WORLD_FOG);
       assertThat(changes.flagsRemoved.get()).isEmpty();
     });
-    assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC), this.bar.flags(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC)).flags());
+    assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC), this.bar.flags(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC)).flags());
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC, Flag.CREATE_WORLD_FOG), this.bar.addFlags(ImmutableSet.of(Flag.CREATE_WORLD_FOG)).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC, BossBar.Flag.CREATE_WORLD_FOG), this.bar.addFlags(ImmutableSet.of(BossBar.Flag.CREATE_WORLD_FOG)).flags());
       assertEquals(7, this.flags.get());
-      assertThat(changes.flagsAdded.get()).containsExactly(Flag.CREATE_WORLD_FOG);
+      assertThat(changes.flagsAdded.get()).containsExactly(BossBar.Flag.CREATE_WORLD_FOG);
       assertThat(changes.flagsRemoved.get()).isEmpty();
     });
 
     // remove flags
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC), this.bar.removeFlag(Flag.CREATE_WORLD_FOG).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC), this.bar.removeFlag(BossBar.Flag.CREATE_WORLD_FOG).flags());
       assertEquals(8, this.flags.get());
       assertThat(changes.flagsAdded.get()).isEmpty();
-      assertThat(changes.flagsRemoved.get()).containsExactly(Flag.CREATE_WORLD_FOG);
+      assertThat(changes.flagsRemoved.get()).containsExactly(BossBar.Flag.CREATE_WORLD_FOG);
     });
-    assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC, Flag.CREATE_WORLD_FOG), this.bar.addFlags(Flag.CREATE_WORLD_FOG).flags());
+    assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC, BossBar.Flag.CREATE_WORLD_FOG), this.bar.addFlags(BossBar.Flag.CREATE_WORLD_FOG).flags());
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC), this.bar.removeFlags(Flag.CREATE_WORLD_FOG).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC), this.bar.removeFlags(BossBar.Flag.CREATE_WORLD_FOG).flags());
       assertEquals(10, this.flags.get());
       assertThat(changes.flagsAdded.get()).isEmpty();
-      assertThat(changes.flagsRemoved.get()).containsExactly(Flag.CREATE_WORLD_FOG);
+      assertThat(changes.flagsRemoved.get()).containsExactly(BossBar.Flag.CREATE_WORLD_FOG);
     });
-    assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC, Flag.CREATE_WORLD_FOG), this.bar.addFlags(Flag.CREATE_WORLD_FOG).flags());
+    assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC, BossBar.Flag.CREATE_WORLD_FOG), this.bar.addFlags(BossBar.Flag.CREATE_WORLD_FOG).flags());
     changes.resetAndThen(() -> {
-      assertEquals(ImmutableSet.of(Flag.PLAY_BOSS_MUSIC), this.bar.removeFlags(ImmutableSet.of(Flag.CREATE_WORLD_FOG)).flags());
+      assertEquals(ImmutableSet.of(BossBar.Flag.PLAY_BOSS_MUSIC), this.bar.removeFlags(ImmutableSet.of(BossBar.Flag.CREATE_WORLD_FOG)).flags());
       assertEquals(12, this.flags.get());
       assertThat(changes.flagsAdded.get()).isEmpty();
-      assertThat(changes.flagsRemoved.get()).containsExactly(Flag.CREATE_WORLD_FOG);
+      assertThat(changes.flagsRemoved.get()).containsExactly(BossBar.Flag.CREATE_WORLD_FOG);
     });
 
     // clear flags
@@ -235,16 +232,16 @@ public class BossBarTest {
       assertEquals(ImmutableSet.of(), this.bar.flags(ImmutableSet.of()).flags());
       assertEquals(13, this.flags.get());
       assertThat(changes.flagsAdded.get()).isEmpty();
-      assertThat(changes.flagsRemoved.get()).containsExactly(Flag.PLAY_BOSS_MUSIC);
+      assertThat(changes.flagsRemoved.get()).containsExactly(BossBar.Flag.PLAY_BOSS_MUSIC);
     });
   }
 
   static class Changes implements BossBar.Listener {
-    final AtomicReference<Set<Flag>> flagsAdded = new AtomicReference<>(emptySet());
-    final AtomicReference<Set<Flag>> flagsRemoved = new AtomicReference<>(emptySet());
+    final AtomicReference<Set<BossBar.Flag>> flagsAdded = new AtomicReference<>(emptySet());
+    final AtomicReference<Set<BossBar.Flag>> flagsRemoved = new AtomicReference<>(emptySet());
 
     @Override
-    public void bossBarFlagsChanged(final @NonNull BossBar bar, final @NonNull Set<Flag> flagsAdded, final @NonNull Set<Flag> flagsRemoved) {
+    public void bossBarFlagsChanged(final @NonNull BossBar bar, final @NonNull Set<BossBar.Flag> flagsAdded, final @NonNull Set<BossBar.Flag> flagsRemoved) {
       this.flagsAdded.set(flagsAdded);
       this.flagsRemoved.set(flagsRemoved);
     }
