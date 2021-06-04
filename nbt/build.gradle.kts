5561c82929403bbc7a16e8f14a62bd150bc998c1
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2021-06-04 12:27:40 +0100

    chore(deps): remove unused checker-qual dependency

--- nbt/build.gradle.kts
+++ nbt/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencies {
   api("net.kyori:examination-api:1.1.0")
   api("net.kyori:examination-string:1.1.0")
-  compileOnlyApi("org.checkerframework:checker-qual:3.13.0")
   compileOnlyApi("org.jetbrains:annotations:21.0.1")
 }
 
