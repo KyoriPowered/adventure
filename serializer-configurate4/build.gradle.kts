00ef6d2befc85944b75b7799dfe44a6715619f5c
zml <zml@stellardrift.ca>
2021-06-21 10:25:21 -0700

    serializer-configurate{3,4}: Bump configurate versions

--- serializer-configurate4/build.gradle.kts
+++ serializer-configurate4/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
   api(project(":adventure-api"))
-  api("org.spongepowered:configurate-core:4.0.0")
+  api("org.spongepowered:configurate-core:4.1.1")
   testImplementation(project(":adventure-text-serializer-gson"))
 }
 
