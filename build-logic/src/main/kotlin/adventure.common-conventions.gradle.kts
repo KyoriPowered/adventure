8baa35e01b3e5f597cc1549cea736d9eb9385cbe
zml <zml@stellardrift.ca>
2021-08-02 09:55:45 -0700

    Clear up some more JMH warnings

--- build-logic/src/main/kotlin/adventure.common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/adventure.common-conventions.gradle.kts
@@ -1,4 +1,5 @@
 import com.adarshr.gradle.testlogger.theme.ThemeType
+import me.champeau.jmh.JMHPlugin
 import me.champeau.jmh.JmhParameters
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition
 
@@ -18,11 +19,14 @@ testlogger {
 
 plugins.withId("me.champeau.jmh") {
   extensions.configure(JmhParameters::class) {
-    jmhVersion.set("1.32")
+    jmhVersion.set(providers.gradleProperty("jmhVersion").forUseAtConfigurationTime())
   }
   tasks.named("compileJmhJava") {
     // avoid implicit task dependencies
     dependsOn(tasks.compileTestJava, tasks.processTestResources)
+  }
+  tasks.named(JMHPlugin.JMH_TASK_COMPILE_GENERATED_CLASSES_NAME, JavaCompile::class) {
+    classpath += configurations.getByName(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME).incoming.files
   }
 }
 
