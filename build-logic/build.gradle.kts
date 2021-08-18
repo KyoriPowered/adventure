667588149685c9921d9cf0ed97dc3282bd721e4c
zml <zml@stellardrift.ca>
2021-08-17 19:20:55 -0700

    build: Bump Gradle version to 7.2

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-  val indraVersion = "2.0.5"
+  val indraVersion = "2.0.6"
   implementation("net.kyori", "indra-common", indraVersion)
   implementation("net.kyori", "indra-publishing-sonatype", indraVersion)
   implementation("com.adarshr", "gradle-test-logger-plugin", "3.0.0")
