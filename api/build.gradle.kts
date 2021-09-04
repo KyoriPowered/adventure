bca8771359f494f4d0e8806d89cfcf2bda702b0a
zml <zml@stellardrift.ca>
2021-09-04 16:34:06 -0700

    build: Bump jetbrains annotations to 22.0.0

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
   api(project(":adventure-key"))
   api("net.kyori:examination-api:1.3.0")
   api("net.kyori:examination-string:1.3.0")
-  compileOnlyApi("org.jetbrains:annotations:21.0.1")
+  compileOnlyApi("org.jetbrains:annotations:22.0.0")
   testImplementation("com.google.guava:guava:23.0")
 }
 
--- key/build.gradle.kts
+++ key/build.gradle.kts
@@ -11,7 +11,7 @@ configurations {
 dependencies {
   api("net.kyori:examination-api:1.3.0")
   api("net.kyori:examination-string:1.3.0")
-  compileOnlyApi("org.jetbrains:annotations:21.0.1")
+  compileOnlyApi("org.jetbrains:annotations:22.0.0")
   testImplementation("com.google.guava:guava:23.0")
 }
 
