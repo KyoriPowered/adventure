kashike/text

api: Audience.forEachAudience and Audience.filterAudience

api/src/main/java/net/kyori/adventure/audience/Audience.java
@@ -24,6 +24,8 @@
 package net.kyori.adventure.audience;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import net.kyori.adventure.bossbar.BossBar;
 import net.kyori.adventure.identity.Identified;
@@ -132,6 +134,34 @@ public interface Audience extends Pointered {
    */
   static @NonNull Collector<? super Audience, ?, ForwardingAudience> toAudience() {
     return Audiences.COLLECTOR;
+  }
+
+  /**
+   * Filters this audience.
+   *
+   * <p>The returned {@code Audience} may be the same, or a completely different one.</p>
+   *
+   * @param filter the filter
+   * @since 4.8.0
+   */
+  default @NonNull Audience filterAudience(final @NonNull Predicate<? super Audience> filter) {
+    if(filter.test(this)) {
+      return this;
+    }
+    return empty();
+  }
+
+  /**
+   * Executes an action against all audiences.
+   *
+   * <p>If you implement {@code Audience} and not {@link ForwardingAudience} in your own code, and your audience forwards to
+   * other audiences, then you <b>must</b> override this method and provide each audience to {@code action}.</p>
+   *
+   * @param action the action
+   * @since 4.8.0
+   */
+  default void forEachAudience(final @NonNull Consumer<? super Audience> action) {
+    action.accept(this);
   }
 
   /**

api/src/main/java/net/kyori/adventure/audience/EmptyAudience.java
@@ -24,6 +24,8 @@
 package net.kyori.adventure.audience;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import net.kyori.adventure.identity.Identified;
 import net.kyori.adventure.identity.Identity;
@@ -49,6 +51,15 @@ final class EmptyAudience implements Audience {
   @Override
   public <T> @PolyNull T getOrDefaultFrom(final @NonNull Pointer<T> pointer, final @NonNull Supplier<? extends T> defaultValue) {
     return defaultValue.get();
+  }
+
+  @Override
+  public @NonNull Audience filterAudience(final @NonNull Predicate<? super Audience> filter) {
+    return this;
+  }
+
+  @Override
+  public void forEachAudience(final @NonNull Consumer<? super Audience> action) {
   }
 
   @Override

api/src/main/java/net/kyori/adventure/audience/ForwardingAudience.java
@@ -23,8 +23,12 @@
  */
 package net.kyori.adventure.audience;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import net.kyori.adventure.bossbar.BossBar;
 import net.kyori.adventure.identity.Identified;
@@ -36,6 +40,7 @@ import net.kyori.adventure.sound.SoundStop;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.checker.nullness.qual.PolyNull;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -72,6 +77,30 @@ public interface ForwardingAudience extends Audience {
   @Override
   default <T> @PolyNull T getOrDefaultFrom(final @NonNull Pointer<T> pointer, final @NonNull Supplier<? extends T> defaultValue) {
     return defaultValue.get(); // unsupported
+  }
+
+  @Override
+  default @NonNull Audience filterAudience(final @NonNull Predicate<? super Audience> filter) {
+    @Nullable List<Audience> audiences = null;
+    for(final Audience audience : this.audiences()) {
+      if(filter.test(audience)) {
+        final Audience filtered = audience.filterAudience(filter);
+        if(filtered != Audience.empty()) {
+          if(audiences == null) {
+            audiences = new ArrayList<>();
+          }
+          audiences.add(filtered);
+        }
+      }
+    }
+    return audiences != null
+      ? Audience.audience(audiences)
+      : Audience.empty();
+  }
+
+  @Override
+  default void forEachAudience(final @NonNull Consumer<? super Audience> action) {
+    for(final Audience audience : this.audiences()) audience.forEachAudience(action);
   }
 
   @Override
@@ -189,6 +218,19 @@ public interface ForwardingAudience extends Audience {
     @Override
     default <T> @PolyNull T getOrDefaultFrom(final @NonNull Pointer<T> pointer, final @NonNull Supplier<? extends T> defaultValue) {
       return this.audience().getOrDefaultFrom(pointer, defaultValue);
+    }
+
+    @Override
+    default @NonNull Audience filterAudience(final @NonNull Predicate<? super Audience> filter) {
+      final Audience audience = this.audience();
+      return filter.test(audience)
+        ? this
+        : Audience.empty();
+    }
+
+    @Override
+    default void forEachAudience(final @NonNull Consumer<? super Audience> action) {
+      this.audience().forEachAudience(action);
     }
 
     @Override

KyoriPowered/adventure

api/src/main/java/net/kyori/adventure/audience/Audience.java
@@ -25,6 +25,8 @@ package net.kyori.adventure.audience;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import net.kyori.adventure.bossbar.BossBar;
 import net.kyori.adventure.identity.Identified;
@@ -134,6 +136,34 @@ public interface Audience extends Pointered {
    */
   static @NotNull Collector<? super Audience, ?, ForwardingAudience> toAudience() {
     return Audiences.COLLECTOR;
+  }
+
+  /**
+   * Filters this audience.
+   *
+   * <p>The returned {@code Audience} may be the same, or a completely different one.</p>
+   *
+   * @param filter the filter
+   * @since 4.8.0
+   */
+  default @NotNull Audience filterAudience(final @NotNull Predicate<? super Audience> filter) {
+    if (filter.test(this)) {
+      return this;
+    }
+    return empty();
+  }
+
+  /**
+   * Executes an action against all audiences.
+   *
+   * <p>If you implement {@code Audience} and not {@link ForwardingAudience} in your own code, and your audience forwards to
+   * other audiences, then you <b>must</b> override this method and provide each audience to {@code action}.</p>
+   *
+   * @param action the action
+   * @since 4.8.0
+   */
+  default void forEachAudience(final @NotNull Consumer<? super Audience> action) {
+    action.accept(this);
   }
 
   /**

api/src/main/java/net/kyori/adventure/audience/EmptyAudience.java
@@ -24,6 +24,8 @@
 package net.kyori.adventure.audience;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import net.kyori.adventure.identity.Identified;
 import net.kyori.adventure.identity.Identity;
@@ -52,6 +54,15 @@ final class EmptyAudience implements Audience {
   @Override
   public <T> @UnknownNullability T getOrDefaultFrom(final @NotNull Pointer<T> pointer, final @NotNull Supplier<? extends T> defaultValue) {
     return defaultValue.get();
+  }
+
+  @Override
+  public @NotNull Audience filterAudience(final @NotNull Predicate<? super Audience> filter) {
+    return this;
+  }
+
+  @Override
+  public void forEachAudience(final @NotNull Consumer<? super Audience> action) {
   }
 
   @Override

api/src/main/java/net/kyori/adventure/audience/ForwardingAudience.java
@@ -23,8 +23,12 @@
  */
 package net.kyori.adventure.audience;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import net.kyori.adventure.bossbar.BossBar;
 import net.kyori.adventure.identity.Identified;
@@ -76,6 +80,30 @@ public interface ForwardingAudience extends Audience {
   @Override
   default <T> @UnknownNullability T getOrDefaultFrom(final @NotNull Pointer<T> pointer, final @NotNull Supplier<? extends T> defaultValue) {
     return defaultValue.get(); // unsupported
+  }
+
+  @Override
+  default @NotNull Audience filterAudience(final @NotNull Predicate<? super Audience> filter) {
+    @Nullable List<Audience> audiences = null;
+    for (final Audience audience : this.audiences()) {
+      if (filter.test(audience)) {
+        final Audience filtered = audience.filterAudience(filter);
+        if (filtered != Audience.empty()) {
+          if (audiences == null) {
+            audiences = new ArrayList<>();
+          }
+          audiences.add(filtered);
+        }
+      }
+    }
+    return audiences != null
+      ? Audience.audience(audiences)
+      : Audience.empty();
+  }
+
+  @Override
+  default void forEachAudience(final @NotNull Consumer<? super Audience> action) {
+    for (final Audience audience : this.audiences()) audience.forEachAudience(action);
   }
 
   @Override
@@ -199,6 +227,19 @@ public interface ForwardingAudience extends Audience {
     @Override
     default <T> @UnknownNullability T getOrDefaultFrom(final @NotNull Pointer<T> pointer, final @NotNull Supplier<? extends T> defaultValue) {
       return this.audience().getOrDefaultFrom(pointer, defaultValue);
+    }
+
+    @Override
+    default @NotNull Audience filterAudience(final @NotNull Predicate<? super Audience> filter) {
+      final Audience audience = this.audience();
+      return filter.test(audience)
+        ? this
+        : Audience.empty();
+    }
+
+    @Override
+    default void forEachAudience(final @NotNull Consumer<? super Audience> action) {
+      this.audience().forEachAudience(action);
     }
 
     @Override