4fb2dad6d36a3d5b69f0087f1cc67ef6e2952c60
zml <zml@stellardrift.ca>
2022-01-04 22:47:09 -0800

    build: update indra to 2.1.0

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
   val indraVersion = "2.1.0"
   implementation("net.kyori", "indra-common", indraVersion)
   implementation("net.kyori", "indra-publishing-sonatype", indraVersion)
+  implementation("net.kyori", "indra-crossdoc", indraVersion)
   implementation("com.adarshr", "gradle-test-logger-plugin", "3.1.0")
   implementation("me.champeau.jmh", "jmh-gradle-plugin", "0.6.6")
   implementation("com.diffplug.gradle", "goomph", "3.35.0")
--- build-logic/src/main/kotlin/adventure.common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/adventure.common-conventions.gradle.kts
@@ -5,6 +5,7 @@ import org.gradle.api.artifacts.type.ArtifactTypeDefinition
 
 plugins {
   id("adventure.base-conventions")
+  id("net.kyori.indra.crossdoc")
   id("net.kyori.indra")
   id("net.kyori.indra.checkstyle")
   id("net.kyori.indra.license-header")
@@ -35,50 +36,13 @@ configurations {
   testCompileClasspath {
     exclude(group = "junit") // brought in by google's libs
   }
-
-  // Register unpacked Javadoc as an artifact for cross-linking
-  javadocElements {
-    outgoing {
-      variants {
-        create("files") {
-          artifact(tasks.javadoc.map { it.destinationDir!! }) {
-            builtBy(tasks.javadoc)
-            type = ArtifactTypeDefinition.DIRECTORY_TYPE // JavaBasePlugin has a hardcoded list of artifact types that can't be published. this is one of them
-          }
-          attributes {
-            attribute(JavadocPackaging.ATTRIBUTE, objects.named(JavadocPackaging.DIRECTORY))
-          }
-        }
-      }
-    }
-  }
-}
-
-// Resolve JD for cross-linking between modules
-val offlineLinkedJavadoc = configurations.register("offlineLinkedJavadoc") {
-  isCanBeResolved = true
-  isCanBeConsumed = false
-
-  attributes {
-    attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
-    attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-    attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.JAVADOC))
-    attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
-    attribute(JavadocPackaging.ATTRIBUTE, objects.named(JavadocPackaging.DIRECTORY))
-  }
-  extendsFrom(configurations.runtimeElements.get())
 }
 
 repositories {
-  mavenLocal()
   mavenCentral()
 }
 
 dependencies {
-  attributesSchema {
-    attribute(JavadocPackaging.ATTRIBUTE)
-  }
-
   annotationProcessor("ca.stellardrift:contract-validator:1.0.1") // https://github.com/zml2008/contract-validator
   api(platform(project(":adventure-bom")))
   checkstyle("ca.stellardrift:stylecheck:0.1")
@@ -91,71 +55,21 @@ dependencies {
   testImplementation("org.junit.jupiter:junit-jupiter-params")
 }
 
-fun filterProjectName(name: String): String {
-  if (name.startsWith(ADVENTURE_PREFIX)) {
-    return name.substring(ADVENTURE_PREFIX.length, name.length)
-  } else {
-    return name
+val ADVENTURE_PREFIX = "adventure-"
+indraCrossdoc {
+  baseUrl().set(providers.gradleProperty("javadocPublishRoot"))
+  nameBasedDocumentationUrlProvider {
+    projectNamePrefix.set(ADVENTURE_PREFIX)
   }
 }
 
 tasks {
-  // link to modules in project
-  val submoduleLinkRoot = providers.gradleProperty("javadocPublishRoot")
-  val jdLinks = offlineLinkedJavadoc.get().incoming
-    .artifactView {
-      componentFilter { it is ProjectComponentIdentifier } // only in-project
-      isLenient = true // ignore artifacts with no javadoc elements variant
-    }
-  val version = project.version
   javadoc {
-    inputs.property("jdLinks.root", submoduleLinkRoot)
-    inputs.property("project.version", version)
-    inputs.files(jdLinks.files)
-      .ignoreEmptyDirectories()
-      .withPropertyName("jdLinks.directories")
-
     val options = options as? StandardJavadocDocletOptions ?: return@javadoc
     options.tags("sinceMinecraft:a:Since Minecraft:")
-
-    doFirst {
-      jdLinks.artifacts.forEach el@{
-        val file = it.file
-        val projectName = (it.id.componentIdentifier as ProjectComponentIdentifier).projectName
-        if (!file.isDirectory) {
-          logger.info("Failed to link to Javadoc in $file (for $projectName) because it was not a directory")
-          return@el
-        }
-
-        // This matches the file structure in adventure-javadocs
-        var linkRoot = submoduleLinkRoot.get()
-        if (!linkRoot.endsWith("/")) {
-          linkRoot += "/"
-        }
-
-        options.linksOffline("${linkRoot}${filterProjectName(projectName)}/$version", file.absolutePath)
-      }
-    }
-  }
-
-  register("copyJavadoc", CopyJavadoc::class) {
-    projectName.set(provider { filterProjectName(project.name) })
-    projectVersion.set(provider { project.version.toString() })
-    javadocFiles.from(javadoc)
-    rootDir.set(project.rootDir)
   }
 
   jacocoTestReport {
     dependsOn(test)
-  }
-  
-  // Non-incremental
-  named("eclipseFactorypath", com.diffplug.gradle.eclipse.apt.GenerateEclipseFactorypath::class) {
-    doFirst {
-      val inFile = inputFile
-      if (inFile != null && inFile.exists()) {
-        inFile.delete()
-      }
-    }
   }
 }
