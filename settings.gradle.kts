454af8d615faa0ca1da8d9002ccdf74fd2af9a4b
zml <zml@stellardrift.ca>
2023-12-16 20:13:18 -0800

    temporarily add snapshot repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,10 @@ dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     mavenCentral()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+      name = "s01Snapshots"
+      mavenContent { snapshotsOnly() }
+    }
   }
 }
 
