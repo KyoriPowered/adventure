34a381a95765d89c7ba48cb67b96f95d5d9d4fc1
codestech <ryszardcichoszotawski@gmail.com>
2024-06-08 13:03:41 +0200

    feat: serialize component children and types

--- text-serializer-nbt/src/main/java/net/kyori/adventure/text/serializer/nbt/NBTSerializerOptions.java
+++ text-serializer-nbt/src/main/java/net/kyori/adventure/text/serializer/nbt/NBTSerializerOptions.java
@@ -5,6 +5,7 @@ import net.kyori.option.Option;
 public final class NBTSerializerOptions {
 
   public static final Option<Boolean> EMIT_COMPACT_TEXT_COMPONENT = Option.booleanOption(key("emit/compact_text_component"), true);
+  public static final Option<Boolean> SERIALIZE_COMPONENT_TYPES = Option.booleanOption(key("serialize/component-types"), true);
 
   private NBTSerializerOptions() {
   }
