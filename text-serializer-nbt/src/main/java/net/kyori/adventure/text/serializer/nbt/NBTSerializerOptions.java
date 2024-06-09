d3e71426a06e13d4678b89a5bd05093cb3e43ee3
codestech <ryszardcichoszotawski@gmail.com>
2024-06-09 12:52:55 +0200

    chore: add support for legacy item tags

--- text-serializer-nbt/src/main/java/net/kyori/adventure/text/serializer/nbt/NBTSerializerOptions.java
+++ text-serializer-nbt/src/main/java/net/kyori/adventure/text/serializer/nbt/NBTSerializerOptions.java
@@ -5,6 +5,14 @@ import net.kyori.option.Option;
 public final class NBTSerializerOptions {
 
   public static final Option<Boolean> EMIT_COMPACT_TEXT_COMPONENT = Option.booleanOption(key("emit/compact_text_component"), true);
+
+  /**
+   * How to emit the item data on {@code show_item} hover events.
+   *
+   * @since 4.18.0
+   */
+  public static final Option<ShowItemHoverDataMode> SHOW_ITEM_HOVER_DATA_MODE = Option.enumOption(key("emit/show_item_hover_data"), ShowItemHoverDataMode.class, ShowItemHoverDataMode.EMIT_EITHER);
+
   public static final Option<Boolean> SERIALIZE_COMPONENT_TYPES = Option.booleanOption(key("serialize/component-types"), true);
 
   private NBTSerializerOptions() {
@@ -12,5 +20,31 @@ public final class NBTSerializerOptions {
 
   private static String key(final String value) {
     return "adventure:nbt/" + value;
+  }
+
+  /**
+   * Configure how to emit show_item hovers.
+   *
+   * @since 4.18.0
+   */
+  public enum ShowItemHoverDataMode {
+    /**
+     * Only emit the pre-1.20.5 item nbt.
+     *
+     * @since 4.18.0
+     */
+    EMIT_LEGACY_NBT,
+    /**
+     * Only emit modern data components.
+     *
+     * @since 4.18.0
+     */
+    EMIT_DATA_COMPONENTS,
+    /**
+     * Emit whichever of legacy or modern data the item has.
+     *
+     * @since 4.18.0
+     */
+    EMIT_EITHER,
   }
 }
