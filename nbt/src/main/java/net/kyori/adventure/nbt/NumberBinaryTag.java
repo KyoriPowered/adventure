293483fc6bfba3caab914eaeb87b9ebaa23d190f
GliczDev <67753196+GliczDev@users.noreply.github.com>
2025-03-11 12:47:22 +0100

    Fix NumberBinaryTag#numberValue javadoc

--- nbt/src/main/java/net/kyori/adventure/nbt/NumberBinaryTag.java
+++ nbt/src/main/java/net/kyori/adventure/nbt/NumberBinaryTag.java
@@ -83,7 +83,7 @@ public interface NumberBinaryTag extends BinaryTag {
   short shortValue();
 
   /**
-   * Gets the value as a {@code Number}
+   * Gets the value as a {@code Number}.
    *
    * @return the value as a {@code Number}
    * @since 4.20.0
