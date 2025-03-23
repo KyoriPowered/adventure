0b031a2c6dbd7ab8e937eaafec7da08a683f411e
zml <zml@stellardrift.ca>
2025-03-22 19:59:17 -0700

    wip

---
+++ dfu/src/main/java/net/kyori/adventure/dfu/AdventureCodecs.java
@@ -0,0 +1,13 @@
+package net.kyori.adventure.dfu;
+
+public final class AdventureCodecs {
+  private AdventureCodecs() {
+  }
+
+  // key
+  // uuid
+  //TextColor
+  // TextDecoration
+  // TranslationArgument
+  // indexed
+}
