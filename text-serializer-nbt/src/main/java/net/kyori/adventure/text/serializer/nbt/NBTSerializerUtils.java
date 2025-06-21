95bd14653bbf1911a3e351ef86cfaf18f9577346
codestech <ryszardcichoszotawski@gmail.com>
2025-06-21 11:05:50 +0200

    chore: update ClickEventSerializer to support the latest format

---
+++ text-serializer-nbt/src/main/java/net/kyori/adventure/text/serializer/nbt/NBTSerializerUtils.java
@@ -0,0 +1,42 @@
+package net.kyori.adventure.text.serializer.nbt;
+
+import net.kyori.adventure.nbt.BinaryTag;
+import net.kyori.adventure.nbt.BinaryTagType;
+import net.kyori.adventure.nbt.CompoundBinaryTag;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+final class NBTSerializerUtils {
+
+  private NBTSerializerUtils() {
+  }
+
+  static <B extends BinaryTag> @NotNull B getRequiredTag(@NotNull CompoundBinaryTag compoundTag,
+                                                         @NotNull String name, @NotNull BinaryTagType<B> tagType) {
+    B tag = getOptionalTag(compoundTag, name, tagType);
+    if (tag == null) {
+      throw new IllegalArgumentException("The specified compound tag does not contain a \"" + name + "\" field");
+    }
+    return (B) tag;
+  }
+
+
+  static <B extends BinaryTag> @Nullable B getOptionalTag(@NotNull CompoundBinaryTag compoundTag,
+                                                          @NotNull String name, @NotNull BinaryTagType<B> tagType) {
+    BinaryTag tag = compoundTag.get(name);
+    if (tag == null) {
+      return null;
+    }
+
+    BinaryTagType<?> actualTagType = tag.type();
+    if (actualTagType != tagType) {
+      throw new IllegalArgumentException(
+        "A type of the tag is different than expected." +
+          " Expected: " + tagType.getClass().getSimpleName() +
+          " Actual: " + actualTagType.getClass().getSimpleName()
+      );
+    }
+
+    return (B) tag;
+  }
+}
