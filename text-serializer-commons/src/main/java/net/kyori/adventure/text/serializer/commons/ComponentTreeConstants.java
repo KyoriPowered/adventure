bc5a7818a4991c9306fb4ced9891d42b8486ac43
codestech <ryszardcichoszotawski@gmail.com>
2025-07-01 03:43:13 +0200

    chore: resolve TODOs

--- text-serializer-commons/src/main/java/net/kyori/adventure/text/serializer/commons/ComponentTreeConstants.java
+++ text-serializer-commons/src/main/java/net/kyori/adventure/text/serializer/commons/ComponentTreeConstants.java
@@ -86,6 +86,8 @@ public final class ComponentTreeConstants {
   @ApiStatus.Obsolete
   public static final String HOVER_EVENT_VALUE = "value";
   @ApiStatus.Obsolete
+  public static final String SHOW_TEXT_TEXT = "text";
+  @ApiStatus.Obsolete
   public static final String SHOW_ENTITY_TYPE = "type";
   public static final String SHOW_ENTITY_ID = "id";
   public static final String SHOW_ENTITY_UUID = "uuid";
