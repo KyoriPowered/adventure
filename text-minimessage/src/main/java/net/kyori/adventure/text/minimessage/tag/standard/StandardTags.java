4b1827fcec6baf7cd0f556f22b40f94b49a30324
Strokkur24 <strokkur.24@gmail.com>
2025-08-22 17:29:36 +0200

    feat(minimessage): add sprite tag

--- text-minimessage/src/main/java/net/kyori/adventure/text/minimessage/tag/standard/StandardTags.java
+++ text-minimessage/src/main/java/net/kyori/adventure/text/minimessage/tag/standard/StandardTags.java
@@ -65,7 +65,8 @@ public final class StandardTags {
         ScoreTag.RESOLVER,
         NbtTag.RESOLVER,
         PrideTag.RESOLVER,
-        ShadowColorTag.RESOLVER
+        ShadowColorTag.RESOLVER,
+        SpriteTag.RESOLVER
       )
       .build();
 
@@ -286,6 +287,16 @@ public final class StandardTags {
    */
   public static @NotNull TagResolver shadowColor() {
     return ShadowColorTag.RESOLVER;
+  }
+
+  /**
+   * Get a resolver for the {@value SpriteTag#SPRITE} tag.
+   *
+   * @return a resolver for the {@value SpriteTag#SPRITE} tag.
+   * @since 4.25.0
+   */
+  public static @NotNull TagResolver spriteTag() {
+    return SpriteTag.RESOLVER;
   }
 
   /**
