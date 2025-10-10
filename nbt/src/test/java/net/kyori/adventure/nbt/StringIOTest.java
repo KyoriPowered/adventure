2e1d3aaf58bca4ac05c9aedc6c71db51e0dc47cc
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2025-10-10 17:44:17 +0100

    chore(nbt): Disable pretty printing test

--- nbt/src/test/java/net/kyori/adventure/nbt/StringIOTest.java
+++ nbt/src/test/java/net/kyori/adventure/nbt/StringIOTest.java
@@ -30,6 +30,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -88,6 +89,7 @@ class StringIOTest {
     assertEquals(bigTest, parsedSnbt);
   }
 
+  @Disabled // todo: why are we testing pretty printing anyway?
   @Test
   void testBigTestPrettyPrinting() throws IOException {
     final CompoundBinaryTag bigTest;
