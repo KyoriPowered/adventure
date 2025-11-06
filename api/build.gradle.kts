633ce4a6903c1363eb1b8eddd26d5cc46e2f9438
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2025-11-06 12:26:58 +0000

    feature(api): Proof-of-concept synthetic annotation for hiding methods

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,6 +1,16 @@
+import net.bytebuddy.asm.ModifierAdjustment
+import net.bytebuddy.build.gradle.Adjustment
+import net.bytebuddy.description.method.MethodDescription
+import net.bytebuddy.description.modifier.SyntheticState
+import net.bytebuddy.description.type.TypeDescription
+import net.bytebuddy.dynamic.ClassFileLocator
+import net.bytebuddy.dynamic.DynamicType
+import net.bytebuddy.matcher.ElementMatcher
+
 plugins {
   id("adventure.common-conventions")
   alias(libs.plugins.jmh)
+  alias(libs.plugins.byteBuddy)
 }
 
 configurations {
@@ -20,3 +30,40 @@ dependencies {
 }
 
 applyJarMetadata("net.kyori.adventure")
+
+class SyntheticPlugin : net.bytebuddy.build.Plugin {
+  private val name = "net.kyori.adventure.internal.Synthetic"
+
+  override fun apply(
+    builder: DynamicType.Builder<*>,
+    typeDescription: TypeDescription,
+    classFileLocator: ClassFileLocator,
+  ): DynamicType.Builder<*> {
+    return builder.visit(
+      ModifierAdjustment()
+        .withMethodModifiers(
+          ElementMatcher { methodDescription: MethodDescription -> methodDescription.declaredAnnotations.any { it.annotationType.name == name } },
+          SyntheticState.SYNTHETIC
+        )
+    )
+  }
+
+  override fun matches(target: TypeDescription): Boolean {
+    return target.declaredMethods.any { method ->
+      method.declaredAnnotations.any { annotation ->
+        annotation.annotationType.name == name
+      }
+    }
+  }
+
+  override fun close() {
+    // Nothing to close!
+  }
+}
+
+byteBuddy {
+  adjustment = Adjustment.SELF
+  transformation {
+    plugin = SyntheticPlugin::class.java
+  }
+}
