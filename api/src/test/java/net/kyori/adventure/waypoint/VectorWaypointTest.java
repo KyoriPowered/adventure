063f782ddef74322d36a10174fa78e70a60824e4
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-04-05 18:06:57 +0200

    fix: spotless checks

--- api/src/test/java/net/kyori/adventure/waypoint/VectorWaypointTest.java
+++ api/src/test/java/net/kyori/adventure/waypoint/VectorWaypointTest.java
@@ -1,11 +1,34 @@
+/*
+ * This file is part of adventure, licensed under the MIT License.
+ *
+ * Copyright (c) 2017-2025 KyoriPowered
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package net.kyori.adventure.waypoint;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Test;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,18 +38,18 @@ public class VectorWaypointTest {
   private final AtomicInteger pos = new AtomicInteger();
   private final Waypoint.Listener listener = new Waypoint.Listener() {
     @Override
-    public void waypointStyleChanged(@NonNull Waypoint waypoint, @NonNull Key oldStyle, @NonNull Key newStyle) {
+    public void waypointStyleChanged(@NonNull final Waypoint waypoint, @NonNull final Key oldStyle, @NonNull final Key newStyle) {
       VectorWaypointTest.this.style.incrementAndGet();
     }
 
     @Override
-    public void waypointColorChanged(@NonNull Waypoint waypoint, @NonNull TextColor oldColor, @NonNull TextColor newColor) {
+    public void waypointColorChanged(@NonNull final Waypoint waypoint, @NonNull final TextColor oldColor, @NonNull final TextColor newColor) {
       VectorWaypointTest.this.color.incrementAndGet();
     }
 
     @Override
     public void waypointVectorChanged(
-      @NonNull VectorWaypoint waypoint,
+      @NonNull final VectorWaypoint waypoint,
       final int oldX,
       final int oldY,
       final int oldZ,
